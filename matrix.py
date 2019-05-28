1ecf8d15b6306845729cd564fd8e67bdcd5226de
Daniel Rines <drines@gmail.com>
2019-05-28 16:26:46 -0700

    init: initial commit for matrix

---
+++ matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PROGRAMMER:       Daniel Rines
+# DATE CREATED:     2019.05.28
+# REVISED DATE:     2019.05.28
+# PURPOSE:  Defines a class for the ISDC's car object.
+#
+# NOTES:    TBD
+#
+#   Example call:
+#      python ...
+##
+
